a5363cc12e3d65068afa5cddbaca3dd137c1fa34
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-10-21 12:06:50 +0200

    Create project base

---
+++ booknest/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const MainComponent = () => {
+  return (
+    <div>
+      <Header/>
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default MainComponent;
