2264ace0fa4ee23ae6ff042a4ae3260e353bced6
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-11-14 16:37:57 +0100

    Update category search

--- src/API/BookService.js
+++ src/API/BookService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export default class BookService {
-  static async getBooks(apiKey) {
+  static async getBooks(apiKey, list) {
       const response = await axios.get(
-        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKey}`
+        `https://api.nytimes.com/svc/books/v3/lists/current/${list}.json?api-key=${apiKey}`
       );
       return response;
   }
--- src/components/pages/BooksList.jsx
+++ src/components/pages/BooksList.jsx
@@ -6,45 +6,32 @@ import BookService from "../../API/BookService";
 const BooksList = ({apiKey}) => {
   const [books, setBooks] = useState([]);
   const [loaded, setLoaded] = useState(false);
-  const [list, setList] = useState(null);
-  const [listEntered, setListEntered] = useState(false);
+  const [list, setList] = useState('list');
+  const [listEntered, setListEntered] = useState('hardcover-fiction');
 
   useEffect(() => {
+    setLoaded(false);
     const fetchBooks = async () => {
-      const res = await BookService.getBooks(apiKey);
+      const res = await BookService.getBooks(apiKey, listEntered);
       setBooks(res.data.results.books);
       setTimeout(() => setLoaded(true), 3000);
     }
   
     fetchBooks();
-  }, []);
+  }, [listEntered]);
 
-  const renderBookList = () => {
-    if(list != null) {
-      return (
-        <div id="list">
+  return (
+    <div id="main-block">
+      <h3 id="find-category">Find your preferable category:</h3>
+      <div id="search">
+          <input id="list-input" type="text" placeholder="Enter category" onChange={(e) => setList(e.target.value)}/>
+          <button id="search-btn" onClick={() => setListEntered(list)}>Search</button>
+      </div>
+      <div id="list">
           {books.map((el, index) => 
             <BooksItem book={el} number={index++} key={index} loaded={loaded}/>
           )}
-        </div>
-      )
-    }
-  }
-
-  const renderListSearch = () => {
-    if(!listEntered) {
-      return (
-        <div id="search">
-          <input type="text" placeholder="Enter category" onChange={(e) => setList(e.target.value)}/>
-          <button onClick={() => setListEntered(true)}>Search</button>
-        </div>
-      )
-    }
-  }
-
-  return (
-    <div>
-      {renderListSearch()}
+      </div>
     </div>
   )
 }
